478329d6fb911c91866283572e9e1cb20816f827
Gregor <kosmina.gregor@gmail.com>
2020-08-25 12:03:38 +0200

    added inline css for emails

--- resources/views/mail/token.blade.php
+++ resources/views/mail/token.blade.php
@@ -1,12 +1,12 @@
 @extends('mail.master')
 @section('content')
     <h3>Ponastavitev gesla</h3>
-    Kopirajte žeton.
-    <b>Po 60 minutah bo žeton neveljaven!</b>
+    Kopirajte žeton:
     <p style="padding-top: 1rem;">
         <b>{{ $token }}</b><br>
     </p>
-    <div class="text-center">
-        <a href="#" class="custom-red">Vnesi žeton</a>
-    </div>
+    <br>
+    <p>
+        <i>Po 60 minutah bo žeton neveljaven!</i>
+    </p>
 @endsection
